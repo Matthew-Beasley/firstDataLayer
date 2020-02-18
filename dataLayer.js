197ec637c318e0b9ce9e34cac17587ae119ec761
conbec <56614504+conbec@users.noreply.github.com>
2020-02-18 15:20:36 -0800

    crud working on server and dataLayer

--- dataLayer.js
+++ dataLayer.js
@@ -1,8 +1,10 @@
 const db = require('pg');
+const uuid = require('uuid/v4');
 const { Client } = db;
 
 const client = new Client('postgres://localhost/cal-poly');
 client.connect();
+
 
 const readCompanies = async () => {
   const sql = 'SELECT * FROM companies';
@@ -10,4 +12,34 @@ const readCompanies = async () => {
   return response;
 }
 
-module.exports = { readCompanies };
+
+const createCompany = async (params) => {
+  const sql = `INSERT INTO companies VALUES 
+    ($1, $2, $3, $4, $5, $6) RETURNING *`;
+
+  const response = await client.query(sql, [uuid(), params.name, params.phone,
+    params.state, params.catchPhrase, params.createdAt]);
+  return response;
+}
+
+
+const updateCompany = async ({ column, value, id }) => {
+  const sql = `UPDATE companies SET ${column} = $1 WHERE id = $2 RETURNING *`; // can't get $1 to work for column name
+  const response = await client.query(sql, [value, id]);
+  return response;
+}
+
+
+const deleteCompany = async ({ id }) => {
+  const sql = 'DELETE FROM companies WHERE id = $1 RETURNING *';
+  const response = await client.query(sql, [id]);
+  return response;
+}
+
+
+module.exports = {
+  readCompanies,
+  createCompany,
+  updateCompany,
+  deleteCompany
+};
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'data')))
 app.use(express.json());
 
+
 app.get('/companies', async (req, res, next) => {
   try {
     const data = await dataLayer.readCompanies();
@@ -20,13 +21,38 @@ app.get('/companies', async (req, res, next) => {
   }
 });
 
+
 app.post('/companies', async (req, res, next) => {
   try {
-
-    const data = datalayer.create(req.body)
+    const data = await dataLayer.createCompany(req.body);
+    res.status(200).send(data.rows);
   }
   catch (ex) {
+    res.status(500).send(ex.message);
+    next(ex);
+  }
+});
 
+
+app.put('/companies', async (req, res, next) => {
+  try {
+    const data = await dataLayer.updateCompany(req.body);
+    res.status(200).send(data);
+  }
+  catch (ex) {
+    res.status(500).send(ex.message);
+    next(ex);
+  }
+});
+
+
+app.delete('/companies', async (req, res, next) => {
+  try {
+    const response = await dataLayer.deleteCompany(req.body);
+    res.status(200).send(response);
+  }
+  catch (ex) {
+    next(ex)
   }
 });
 
